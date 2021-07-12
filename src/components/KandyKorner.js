0e4b14d9ba06140c180f5dbc47fabba0d980999d
sxiong0519 <sxiong0519@gmail.com>
2021-07-12 11:43:38 -0500

    added kandykorner.js

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+import "./KandyKorner.css"
+
+export const KandyKorner = () => (
+   <> 
+        <NavBar />
+        <ApplicationViews />
+
+    </>
+)
+
